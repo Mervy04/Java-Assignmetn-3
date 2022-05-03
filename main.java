ef09aaafc22ae917d99c58ab301e7b3b938cc57d
Mervy04 <87144944+Mervy04@users.noreply.github.com>
2022-05-03 10:11:54 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import static com.company.PERT.*;
+public class Main {
+
+    public static void main(String[] args) {
+        PERT pert = new PERT();
+        pert.reader();
+    }
+
+
+
+
+
+
+}
